74c9b3731560573ecd4c41286fc6dce2c456c993
Houssam-eddine Moumanis <hmoumani@e1r13p12.1337.ma>
2020-12-13 20:25:41 +0100

    let's start the magic

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+// int ft_strlen(char *s);
+int ft_strcmp(char *s, char *s1);
+
+
+int main()
+{   
+    char *s = "1";
+    char *s1 = "9";
+    printf("%d\n", ft_strcmp(s, s1));
+    // printf("%p | %p | %p", s, s1, s2);
+}
