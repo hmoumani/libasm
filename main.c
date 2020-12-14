15d36dbe4b326a08d9f727dfa78702642319fef3
Houssam-eddine Moumanis <hmoumani@e1r13p12.1337.ma>
2020-12-14 16:03:07 +0100

    iscii prob

--- main.c
+++ main.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h> 
+#include <unistd.h>
 // int ft_strlen(char *s);
-int ft_strcmp(char *s, char *s1);
-
-
+// int ft_strcmp(char *s, char *s1);
+ssize_t ft_read(int fildes, void *buf, size_t nbyte);
 int main()
 {   
-    char *s = "1";
-    char *s1 = "9";
-    printf("%d\n", ft_strcmp(s, s1));
-    // printf("%p | %p | %p", s, s1, s2);
+    int fd = 2;
+    int number = 20;
+    char *s = malloc(sizeof(char) * 20);
+    // int r = ft_read(fd, s, number);
+    // printf("\n   *    ");
+    // printf("%d **** ", r);
+    int r1 = ft_read(fd, s, number);
+    printf("%d", r1);
+    s[r1] = 0;
+    printf("%s", s);
+    free(s);
 }
